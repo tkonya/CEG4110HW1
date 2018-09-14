f66dda9874fac2d543ffa1f8ad13a0adf259cf8c
T Konya <trevor.konya@gmail.com>
2018-09-14 18:02:21 -0400

    Integrated https://github.com/Pes8/android-material-color-picker-dialog
    Got multiple paths kind of working, it erases previous path when drawing with a new color though

--- app/src/main/java/io/konya/ceg4110hw1/CustomDrawingView.kt
+++ app/src/main/java/io/konya/ceg4110hw1/CustomDrawingView.kt
@@ -1,12 +1,15 @@
 package io.konya.ceg4110hw1
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.Path
 import android.graphics.drawable.Drawable
 import android.text.TextPaint
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 
 /**
@@ -14,13 +17,16 @@ import android.view.View
  */
 class CustomDrawingView : View {
 
-    private var _exampleString: String? = null // TODO: use a default from R.string...
-    private var _exampleColor: Int = Color.RED // TODO: use a default from R.color...
-    private var _exampleDimension: Float = 0f // TODO: use a default from R.dimen...
+    private var _exampleString: String? = null
+    private var _exampleColor: Int = Color.RED
+    private var _exampleDimension: Float = 0f
 
     private var textPaint: TextPaint? = null
     private var textWidth: Float = 0f
     private var textHeight: Float = 0f
+
+    private var paint = Paint()
+    private var paths = mutableListOf<Path>()
 
     /**
      * The text to draw
@@ -59,14 +65,31 @@ class CustomDrawingView : View {
 
     constructor(context: Context) : super(context) {
         init(null, 0)
+        initializeState()
     }
 
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
         init(attrs, 0)
+        initializeState()
     }
 
     constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle) {
         init(attrs, defStyle)
+        initializeState()
+    }
+
+    private fun initializeState() {
+        paint.isAntiAlias = true
+        paint.strokeWidth = 20f
+        paint.color = Color.BLACK
+        paint.style = Paint.Style.STROKE
+        paint.strokeJoin = Paint.Join.ROUND
+        paths.add(Path())
+    }
+
+    fun setPaintColor(alpha: Int, red: Int, green: Int, blue: Int) {
+        paint.color = Color.argb(alpha, red, green, blue)
+        paths.add(Path())
     }
 
     private fun init(attrs: AttributeSet?, defStyle: Int) {
@@ -112,10 +135,16 @@ class CustomDrawingView : View {
         }
     }
 
+    @SuppressLint("DrawAllocation")
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
-        // TODO: consider storing these as member variables to reduce allocations per draw cycle.
+//        if (paths.size == 0) {
+//            paths.add(Path())
+//        }
+        canvas.drawPath(paths[paths.lastIndex], paint)
+
+
         val paddingLeft = paddingLeft
         val paddingTop = paddingTop
         val paddingRight = paddingRight
@@ -138,5 +167,31 @@ class CustomDrawingView : View {
                     paddingLeft + contentWidth, paddingTop + contentHeight)
             it.draw(canvas)
         }
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+
+        var eventX = event?.x
+        var eventY = event?.y
+
+        if (eventX == null || eventY == null) {
+            return super.onTouchEvent(event)
+        }
+
+        when (event?.action) {
+            MotionEvent.ACTION_DOWN -> {
+                paths[paths.lastIndex].moveTo(eventX, eventY)
+                return true
+            }
+            MotionEvent.ACTION_MOVE -> {
+                paths[paths.lastIndex].lineTo(eventX, eventY)
+                invalidate()
+                return true
+            }
+            else -> {
+                return false
+            }
+        }
+
     }
 }
--- app/src/main/java/io/konya/ceg4110hw1/DrawActivity.kt
+++ app/src/main/java/io/konya/ceg4110hw1/DrawActivity.kt
@@ -1,12 +1,53 @@
 package io.konya.ceg4110hw1
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.pes.androidmaterialcolorpickerdialog.ColorPicker
+import com.pes.androidmaterialcolorpickerdialog.ColorPickerCallback
+import android.support.annotation.ColorInt
+import android.view.View
+
 
 class DrawActivity : AppCompatActivity() {
+
+    var colorRed = 150
+    var colorGreen = 150
+    var colorBlue = 150
+    var colorAlpha = 1
+
+    lateinit var drawView : CustomDrawingView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_draw)
+
+        drawView = findViewById<CustomDrawingView>(R.id.custom_draw_view_id)
+
+        val selectColorButton = findViewById<Button>(R.id.select_color_button_id)
+        selectColorButton.setOnClickListener {
+            val colorPicker = ColorPicker(this, 150, 150, 150)
+            colorPicker.show()
+
+            colorPicker.setCallback { color ->
+                // Do whatever you want
+                // Examples
+                colorAlpha = Color.alpha(color)
+                colorRed = Color.red(color)
+                colorGreen = Color.green(color)
+                colorBlue = Color.blue(color)
+
+                drawView.setPaintColor(colorAlpha, colorRed, colorGreen, colorBlue)
+
+                colorPicker.dismiss()
+            }
+
+        }
+
+        val saveImageButton = findViewById<Button>(R.id.save_drawing_button_id)
+        saveImageButton.setOnClickListener {
+            println("It's something!")
+        }
     }
 }
